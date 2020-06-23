3ab0e9e22c6fedcd1bc9ab99c6f68ba60038794b
dongan2708 <65014848+dongan2708@users.noreply.github.com>
2020-06-23 12:47:59 +0700

    SS12+13

---
+++ Session12/ex6.js
@@ -0,0 +1,15 @@
+function numericonly()
+{
+    if(!eventt.keyCode >=48 && event.keyCode <=57)
+        event.returnValue=false;
+}
+function countWords() {
+    var message = document.getElementBy('txtMessage').value;
+    message = message.replace(/\s+/g,'');
+    var numberOfWords = message.split('').length;
+    document.getElementById('txtTrack').value = "words Remaining:"+
+        eval(50 - numberOfWords);
+    if (numberOfWords > 50)
+        alert("too many words.");
+
+}
